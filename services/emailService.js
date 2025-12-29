233d22704fcf2aba152597ec4a88288cdda70201
mohamedaliarafat <112913473+mohamedaliarafat@users.noreply.github.com>
2025-12-29 19:36:20 +0300

    Update emailService.js

--- services/emailService.js
+++ services/emailService.js
@@ -13,7 +13,7 @@ const resend = new Resend(process.env.RESEND_API_KEY);
 exports.sendEmail = async ({ to, subject, html }) => {
   try {
     const response = await resend.emails.send({
-      from: process.env.EMAIL_FROM || "ALBUHAIRA <no-reply@yourdomain.com>",
+      from: process.env.EMAIL_FROM || "ALBUHAIRA <no-reply@resend.dev>",
       to,
       subject,
       html,
