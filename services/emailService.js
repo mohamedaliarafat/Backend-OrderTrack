4148b5f7a5cdf7077441049aa0cd74045adb8347
mohamedaliarafat <112913473+mohamedaliarafat@users.noreply.github.com>
2025-12-29 19:21:23 +0300

    Update emailService.js

--- services/emailService.js
+++ services/emailService.js
@@ -2,15 +2,17 @@ const nodemailer = require('nodemailer');
 
 const transporter = nodemailer.createTransport({
   host: process.env.EMAIL_HOST,
-  port: process.env.EMAIL_PORT,
-  secure: false, // true لو 465
+  port: 465,
+  secure: true, // مهم مع Render
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS,
   },
+  connectionTimeout: 10000,
 });
 
-const sendEmail = async ({ to, subject, html }) => {
+// دالة إرسال إيميل عامة
+exports.sendEmail = async ({ to, subject, html }) => {
   try {
     const info = await transporter.sendMail({
       from: process.env.EMAIL_FROM,
@@ -20,11 +22,9 @@ const sendEmail = async ({ to, subject, html }) => {
     });
 
     console.log('📧 Email sent:', info.messageId);
-    return true;
+    return info;
   } catch (error) {
     console.error('❌ Email error:', error);
-    return false;
+    throw error;
   }
 };
-
-module.exports = { sendEmail };
