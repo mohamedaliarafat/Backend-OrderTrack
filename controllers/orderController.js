b262e0508b76a55f007587182da867e26447b7d8
Mohamed Ali Arafat <112913473+mohamedaliarafat@users.noreply.github.com>
2026-01-03 16:53:34 +0300

    Update orderController.js

--- controllers/orderController.js
+++ controllers/orderController.js
@@ -7,6 +7,8 @@ const EmailTemplates = require('../services/emailTemplates');
 const getOrderEmails = require('../utils/getOrderEmails');
 const Activity = require('../models/Activity');
 const Notification = require('../models/Notification');
+const { safeSendEmail } = require('../services/emailQueue');
+
 const mongoose = require('mongoose');
 const multer = require('multer');
 const path = require('path');
