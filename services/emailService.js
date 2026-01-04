f5e12157876ff2a03c8b41660da6def830a7deb5
Mohamed Ali Arafat <112913473+mohamedaliarafat@users.noreply.github.com>
2026-01-04 18:03:25 +0300

    Update emailService.js

--- services/emailService.js
+++ services/emailService.js
@@ -5,13 +5,17 @@ const nodemailer = require('nodemailer');
 // ===============================
 const transporter = nodemailer.createTransport({
   host: 'mail-eu.smtp2go.com',
-  port: 587, // أو 2525
-  secure: false, // TLS
+  port: 2525, // 🔥 غيرنا البورت
+  secure: false,
   auth: {
-    user: process.env.SMTP_USER, // albuheiraalarabia.com
-    pass: process.env.SMTP_PASS, // SMTP password
+    user: process.env.SMTP_USER,
+    pass: process.env.SMTP_PASS,
   },
+  connectionTimeout: 10000,
+  greetingTimeout: 10000,
+  socketTimeout: 10000,
 });
+
 
 // ===============================
 // 📧 Email Constants
